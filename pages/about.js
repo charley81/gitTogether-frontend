241d94615ac1740e8715ffe6c808aa2cfd0c3460
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-12 12:02:52 -0400

    created about page and initial jsx for it

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        gitTogether allows you to view your neighborhood get togethers. View all
+        events in your neighborhood as well as create an account and post
+        events.
+      </p>
+    </div>
+  )
+}
