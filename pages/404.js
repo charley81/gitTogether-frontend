c3c28355720331eee64550a0ef3e59a360a927d8
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-13 09:49:01 -0400

    error page complete

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Layout from '../components/layout'
+import styles from '../styles/error.module.scss'
+
+export default function ErrorPage() {
+  return (
+    <Layout page="Error Page" description="Page Not Found">
+      <section className={styles.error}>
+        <h1>Page Not Found</h1>
+        <Link href="/">
+          <a className="btn">Back Home</a>
+        </Link>
+      </section>
+    </Layout>
+  )
+}
